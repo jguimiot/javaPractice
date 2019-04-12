4a0fb15adc59f49a2e3179355c039bd47bc6547c
jguimiot <selljm14@gmail.com>
2019-04-12 10:04:52 -0500

    Add files via upload
    This code comes from a YouTube tutorial by Olav3D, linked below.  I'll use it to create a data visualization about the housing market.
    
    https://www.youtube.com/watch?v=KI0tjZUkb5A&t=34s

---
+++ blocks.py
@@ -0,0 +1,13 @@
+import bpy
+
+number = 10
+counter = 0
+for a in range(0,number):
+    counter += 2
+    counter2 = 0
+    for b in range(0,number):
+        counter2 += 2
+        counter3 = 0
+        for c in range (0,number):
+            
+            
